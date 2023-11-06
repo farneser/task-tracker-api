0d206f08eed4e84d8362a70b91bd82e26042497d
farneser <farneser1@gmail.com>
2023-11-07 01:30:59 +0300

    feat: jwt utils

---
+++ src/main/java/dev/farneser/tasktracker/api/utils/JwtUtils.java
@@ -0,0 +1,58 @@
+package dev.farneser.tasktracker.api.utils;
+
+import dev.farneser.tasktracker.api.models.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtils {
+
+    @Value("${jwt.secret-key}")
+    private static String SECRET_KEY;
+    private static final long JWT_EXPIRATION_MS = 60 * 60 * 1000;
+
+    public String generateToken(User userDetails) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + JWT_EXPIRATION_MS);
+
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .claim("authorities", userDetails.getAuthorities())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(getSignInKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSignInKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    private Key getSignInKey() {
+        var keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
