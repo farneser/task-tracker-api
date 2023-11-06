923006d4c45c72d368f8c5176ae3231f3c76f105
farneser <farneser1@gmail.com>
2023-11-06 23:42:51 +0300

    feat: initial

---
+++ src/test/java/dev/farneser/tasktracker/api/TaskTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.farneser.tasktracker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
