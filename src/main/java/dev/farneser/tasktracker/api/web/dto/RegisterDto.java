4ce10416a83a5b9c710a354613dc8c0a93b56bc8
farneser <farneser1@gmail.com>
2023-11-08 19:24:57 +0300

    feat: register dto

---
+++ src/main/java/dev/farneser/tasktracker/api/web/dto/RegisterDto.java
@@ -0,0 +1,19 @@
+package dev.farneser.tasktracker.api.web.dto;
+
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+import lombok.NonNull;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+public class RegisterDto {
+    @NonNull
+    @Email(message = "You should enter email like 'example@email.com'")
+    private String email;
+    @NonNull
+    @Length(min = 8, max = 64)
+    private String confirmPassword;
+    @NonNull
+    @Length(min = 8, max = 64)
+    private String password;
+}
