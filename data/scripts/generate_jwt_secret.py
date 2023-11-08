be0efcb00019515cac9b6751f65cdc215575ea85
farneser <farneser1@gmail.com>
2023-11-09 00:25:53 +0300

    fix: jwt secret generator, application-dev.properties

--- data/scripts/generate_jwt_secret.py
+++ data/scripts/generate_jwt_secret.py
@@ -1,8 +1,8 @@
 def generate_jwt_secret(size: int = 32) -> str:
     import secrets
 
-    return secrets.token_hex(size)
+    return f"'{secrets.token_hex(size)}'".upper()
 
 
 if __name__ == "__main__":
-    print(f"Generated jwt secret key 32 byte size: ${generate_jwt_secret()}")
+    print(f"Generated jwt secret key 32-byte (256-bit) size: {generate_jwt_secret()}")
