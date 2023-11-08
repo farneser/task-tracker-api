8d58a33a10134035b4443cdf5fce9716d9a28b9d
farneser <farneser1@gmail.com>
2023-11-08 19:25:13 +0300

    feat: user dto and readme

---
+++ src/main/java/dev/farneser/tasktracker/api/web/dto/UserDto.java
@@ -0,0 +1,10 @@
+package dev.farneser.tasktracker.api.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserDto {
+    private String email;
+}
