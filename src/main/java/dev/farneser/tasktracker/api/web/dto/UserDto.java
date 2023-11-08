787c62df0fd3e6f6c62da5408e4e21c278bb2517
farneser <farneser1@gmail.com>
2023-11-08 19:29:29 +0300

    feat: auth controller

--- src/main/java/dev/farneser/tasktracker/api/web/dto/UserDto.java
+++ src/main/java/dev/farneser/tasktracker/api/web/dto/UserDto.java
@@ -2,9 +2,11 @@ package dev.farneser.tasktracker.api.web.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
     private String email;
 }
