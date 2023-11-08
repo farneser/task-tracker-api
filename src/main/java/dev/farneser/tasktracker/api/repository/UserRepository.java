623ac387699c50dd2690f8e2540bf3e8458088cd
farneser <farneser1@gmail.com>
2023-11-09 01:07:25 +0300

    feat: user service register new user method

--- src/main/java/dev/farneser/tasktracker/api/repository/UserRepository.java
+++ src/main/java/dev/farneser/tasktracker/api/repository/UserRepository.java
@@ -3,6 +3,8 @@ package dev.farneser.tasktracker.api.repository;
 import dev.farneser.tasktracker.api.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/dev/farneser/tasktracker/api/service/UserService.java
+++ src/main/java/dev/farneser/tasktracker/api/service/UserService.java
@@ -1,29 +1,52 @@
 package dev.farneser.tasktracker.api.service;
 
+import dev.farneser.tasktracker.api.models.User;
 import dev.farneser.tasktracker.api.repository.UserRepository;
+import dev.farneser.tasktracker.api.web.dto.RegisterDto;
+import dev.farneser.tasktracker.api.web.dto.UserDto;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, ModelMapper modelMapper) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.modelMapper = modelMapper;
+    }
+
+    public UserDto registerNewUser(@Valid RegisterDto registerDto) {
+        try {
+            var user = new User();
+
+            user.setEmail(registerDto.getEmail());
+            user.setPassword(passwordEncoder.encode(registerDto.getEmail()));
+
+            user = userRepository.save(user);
+
+            return modelMapper.map(user, UserDto.class);
+
+        } catch (Exception e) {
+
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        var user = userRepository.findByEmail(email);
-
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found");
-        }
-
-        return user;
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }
