29779572b6e67e0497d294bbe56861c222980c7c
farneser <farneser1@gmail.com>
2023-11-09 03:10:43 +0300

    fix: user dto

--- src/main/java/dev/farneser/tasktracker/api/web/dto/UserDto.java
+++ src/main/java/dev/farneser/tasktracker/api/web/dto/UserDto.java
@@ -1,12 +1,25 @@
 package dev.farneser.tasktracker.api.web.dto;
 
+import dev.farneser.tasktracker.api.config.mapping.ITypeMapper;
+import dev.farneser.tasktracker.api.models.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserDto {
+@Component
+public class UserDto implements ITypeMapper {
+    private long id;
     private String email;
+
+    public void mapping(ModelMapper modelMapper) {
+        modelMapper.createTypeMap(User.class, UserDto.class)
+                .addMapping(User::getId, UserDto::setId)
+                .addMapping(User::getEmail, UserDto::setEmail);
+
+    }
 }
