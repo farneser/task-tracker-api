7d8d4e234f45c1dbe008cbd2a4884539c126d212
farneser <farneser1@gmail.com>
2023-11-10 22:47:22 +0300

    feat: generate refresh token method

--- src/main/java/dev/farneser/tasktracker/api/service/JwtService.java
+++ src/main/java/dev/farneser/tasktracker/api/service/JwtService.java
@@ -20,43 +20,57 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
+    // FIXME: 11/10/23 get time via env
     // 1000 * 3600 * 48 equals two days of token lifetime
     private static final long JWT_EXPIRATION_MS = 1000 * 3600 * 48;
+
+    // 1000 * 3600 * 24 * 14 equals two weeks of token lifetime
+    private static final long REFRESH_TOKEN_EXPIRATION_MS = 1000 * 3600 * 24 * 14;
     @Value("${jwt.secret}")
     private String secretKey;
 
     /**
-     * Generates a JWT token for the provided UserDetails.
+     * Generates a JWT for the provided UserDetails.
      *
      * @param userDetails The UserDetails object containing user information.
-     * @return A JWT token as a String.
+     * @return A JWT as a String.
      */
-    public String generateToken(UserDetails userDetails) {
-        return this.generateToken(new HashMap<>(), userDetails);
+    public String generateAccessToken(UserDetails userDetails) {
+        return this.generateToken(new HashMap<>(), userDetails, JWT_EXPIRATION_MS);
     }
 
     /**
-     * Generates a JWT token with additional custom claims for the provided UserDetails.
+     * Generates a Refresh token for the provided UserDetails.
+     *
+     * @param userDetails The UserDetails object containing user information.
+     * @return A Refresh token as a String.
+     */
+    public String generateRefreshToken(UserDetails userDetails) {
+        return this.generateToken(new HashMap<>(), userDetails, REFRESH_TOKEN_EXPIRATION_MS);
+    }
+
+    /**
+     * Generates a JWT with additional custom claims for the provided UserDetails.
      *
      * @param extraClaims Additional custom claims to include in the JWT.
      * @param userDetails The UserDetails object containing user information.
-     * @return A JWT token as a String with specified custom claims.
+     * @return A JWT as a String with specified custom claims.
      */
-    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails, long expiration) {
         return Jwts
                 .builder()
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION_MS))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
     /**
-     * Checks if a JWT token is valid for the given UserDetails.
+     * Checks if a JWT is valid for the given UserDetails.
      *
-     * @param token       The JWT token to be validated.
+     * @param token       The JWT to be validated.
      * @param userDetails The UserDetails object for the user.
      * @return True if the token is valid for the provided user, false otherwise.
      */
@@ -66,29 +80,29 @@ public class JwtService {
     }
 
     /**
-     * Checks if a JWT token has expired.
+     * Checks if a JWT has expired.
      *
-     * @param token The JWT token to be checked for expiration.
+     * @param token The JWT to be checked for expiration.
      * @return True if the token has expired, false otherwise.
      */
     private boolean isTokenExpired(String token) {
-        return extraExpiration(token).before(new Date());
+        return extractExpiration(token).before(new Date());
     }
 
     /**
-     * Extracts the expiration date from a JWT token.
+     * Extracts the expiration date from a JWT.
      *
-     * @param token The JWT token from which to extract the expiration date.
+     * @param token The JWT from which to extract the expiration date.
      * @return The expiration date of the token.
      */
-    private Date extraExpiration(String token) {
+    private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
     /**
-     * This function extracts the username from a given JWT token
+     * This function extracts the username from a given JWT
      *
-     * @param token The JWT token from which the username is to be extracted
+     * @param token The JWT from which the username is to be extracted
      * @return The extracted username
      */
     public String extractUsername(String token) {
@@ -96,15 +110,15 @@ public class JwtService {
     }
 
     /**
-     * This generic function allows for the extraction of custom claims from a JWT token using a specified
+     * This generic function allows for the extraction of custom claims from a JWT using a specified
      * claims resolver
      *
-     * @param token          The JWT token from which the claim is to be extracted
+     * @param token          The JWT from which the claim is to be extracted
      * @param claimsResolver A functional interface representing the logic to extract a specific claim from
      *                       JWT claims
      * @param <T>            The extracted claim as per the specified type, providing flexibility to extract
-     *                       different types of claims from the JWT token
-     * @return All claims present in the JWT token
+     *                       different types of claims from the JWT
+     * @return All claims present in the JWT
      */
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         var claims = extractClaims(token);
@@ -113,10 +127,10 @@ public class JwtService {
     }
 
     /**
-     * Method that extract claims form JWT token
+     * Method that extract claims form JWT
      *
-     * @param token Defile jwt token as String
-     * @return All claims in a jwt token
+     * @param token Defile jwt as String
+     * @return All claims in a jwt
      */
     private Claims extractClaims(String token) {
         return Jwts
@@ -128,7 +142,7 @@ public class JwtService {
     }
 
     /**
-     * This private method retrieves the signing key used for JWT token validation
+     * This private method retrieves the signing key used for JWT validation
      *
      * @return The signing key derived from the provided secret key
      */
