d473b58fadd00925267e9c89d7b2bd93b3ad1b4e
farneser <farneser1@gmail.com>
2023-11-11 00:46:28 +0300

    feat: swagger

--- src/main/java/dev/farneser/tasktracker/api/config/SecurityConfig.java
+++ src/main/java/dev/farneser/tasktracker/api/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
 
                             req.requestMatchers(
                                     "/swagger-ui.html",
+                                    "/swagger-ui/**",
                                     "/v3/api-docs",
                                     "/v3/api-docs/swagger-config"
                             ).permitAll();
