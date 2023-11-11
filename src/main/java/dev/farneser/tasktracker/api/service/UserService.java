0a1e29f7a732ce51d554306e6f0fa71361c7e68a
farneser <farneser1@gmail.com>
2023-11-11 17:14:08 +0300

    feat: user controller get /user method

--- src/main/java/dev/farneser/tasktracker/api/service/UserService.java
+++ src/main/java/dev/farneser/tasktracker/api/service/UserService.java
@@ -1,8 +1,12 @@
 package dev.farneser.tasktracker.api.service;
 
+import dev.farneser.tasktracker.api.models.User;
 import dev.farneser.tasktracker.api.repository.UserRepository;
+import dev.farneser.tasktracker.api.web.dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,9 +17,22 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+
+    private User getByEmail(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return getByEmail(email);
+    }
+
+    public UserDto getUser(Authentication authentication) {
+        var username = authentication.getName();
+
+        var user = getByEmail(username);
+
+        return modelMapper.map(user, UserDto.class);
     }
 }
---
+++ src/main/java/dev/farneser/tasktracker/api/web/controllers/UserController.java
@@ -0,0 +1,24 @@
+package dev.farneser.tasktracker.api.web.controllers;
+
+
+import dev.farneser.tasktracker.api.service.UserService;
+import dev.farneser.tasktracker.api.web.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    @GetMapping
+    public ResponseEntity<UserDto> get(Authentication authentication) {
+        return ResponseEntity.ok(userService.getUser(authentication));
+    }
+}
