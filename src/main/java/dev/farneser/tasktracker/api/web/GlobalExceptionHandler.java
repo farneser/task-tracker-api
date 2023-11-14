307c02ca45f6ed2b93078a5e26aeaf1cfafb2fcf
farneser <farneser1@gmail.com>
2023-11-14 19:03:03 +0300

    feat: bad credentials handler

--- src/main/java/dev/farneser/tasktracker/api/web/GlobalExceptionHandler.java
+++ src/main/java/dev/farneser/tasktracker/api/web/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -22,30 +23,37 @@ import java.util.Objects;
 @RestController
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<ErrorResponse> handler(BadCredentialsException ex) {
+        log.error(ex.getMessage());
+        return getResponseEntity(HttpStatus.UNAUTHORIZED, ex.getMessage());
+    }
+
     @ExceptionHandler(InvalidTokenException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ResponseEntity<ErrorResponse> handleInvalidToken(InvalidTokenException ex) {
+    public ResponseEntity<ErrorResponse> handler(InvalidTokenException ex) {
         log.error(ex.getMessage());
         return getResponseEntity(HttpStatus.UNAUTHORIZED, ex.getMessage());
     }
 
     @ExceptionHandler(TokenExpiredException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ResponseEntity<ErrorResponse> handleTokenExpired(TokenExpiredException ex) {
+    public ResponseEntity<ErrorResponse> handler(TokenExpiredException ex) {
         log.error(ex.getMessage());
         return getResponseEntity(HttpStatus.UNAUTHORIZED, ex.getMessage());
     }
 
     @ExceptionHandler(ExpiredJwtException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ResponseEntity<ErrorResponse> handleExpiredJwt(ExpiredJwtException ex) {
+    public ResponseEntity<ErrorResponse> handler(ExpiredJwtException ex) {
         log.error(ex.getMessage());
         return getResponseEntity(HttpStatus.UNAUTHORIZED, ex.getMessage());
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-    public ResponseEntity<ErrorResponse> handleMediaType(HttpMessageNotReadableException ex) {
+    public ResponseEntity<ErrorResponse> handler(HttpMessageNotReadableException ex) {
         log.error(ex.getMessage());
 
         return getResponseEntity(HttpStatus.UNSUPPORTED_MEDIA_TYPE, ex.getMessage());
@@ -53,21 +61,21 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<ErrorResponse> handleValidation(MethodArgumentNotValidException ex) {
+    public ResponseEntity<ErrorResponse> handler(MethodArgumentNotValidException ex) {
         log.error(ex.getMessage());
         return getResponseEntity(HttpStatus.BAD_REQUEST, Objects.requireNonNull(ex.getFieldError()).getDefaultMessage());
     }
 
     @ExceptionHandler(UsernameNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<ErrorResponse> handleUsernameNotFound(UsernameNotFoundException ex) {
+    public ResponseEntity<ErrorResponse> handler(UsernameNotFoundException ex) {
         log.error(ex.getMessage());
         return getResponseEntity(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<ErrorResponse> handleAll(Exception ex) {
+    public ResponseEntity<ErrorResponse> handler(Exception ex) {
         log.error(ex.getMessage());
         return getResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
     }
