1eb9c3f6725d3eb1278a16c698e4595cb77f5a53
farneser <farneser1@gmail.com>
2023-11-21 20:47:09 +0300

    feat: task order number

--- src/main/resources/db/migration/V04__Create_Task_Table.sql
+++ src/main/resources/db/migration/V04__Create_Task_Table.sql
@@ -1,10 +1,11 @@
 CREATE TABLE tasks
 (
-    id          SERIAL PRIMARY KEY,
-    task_name   VARCHAR(255),
-    description VARCHAR(255),
-    user_id     BIGINT,
-    column_id   BIGINT,
+    id           SERIAL PRIMARY KEY,
+    task_name    VARCHAR(255),
+    description  VARCHAR(255),
+    user_id      BIGINT,
+    column_id    BIGINT,
+    order_number BIGINT,
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (column_id) REFERENCES columns (id)
 );
