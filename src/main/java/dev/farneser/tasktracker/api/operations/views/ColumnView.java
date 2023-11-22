8139f6ea0efded46794cb98bc63563f9ba1cd21e
farneser <farneser1@gmail.com>
2023-11-23 02:59:32 +0300

    feat: task and column views inside column and task

--- src/main/java/dev/farneser/tasktracker/api/operations/views/ColumnView.java
+++ src/main/java/dev/farneser/tasktracker/api/operations/views/ColumnView.java
@@ -5,18 +5,22 @@ import dev.farneser.tasktracker.api.models.KanbanColumn;
 import lombok.Data;
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
+
 @Data
 public class ColumnView implements ITypeMapper {
     private Long id;
     private String columnName;
     private Boolean isCompleted;
     private Long orderNumber;
+    private List<TaskView> tasks;
 
     public void mapping(ModelMapper modelMapper) {
         modelMapper.createTypeMap(KanbanColumn.class, ColumnView.class)
                 .addMapping(KanbanColumn::getId, ColumnView::setId)
                 .addMapping(KanbanColumn::getColumnName, ColumnView::setColumnName)
                 .addMapping(KanbanColumn::getIsCompleted, ColumnView::setIsCompleted)
-                .addMapping(KanbanColumn::getOrderNumber, ColumnView::setOrderNumber);
+                .addMapping(KanbanColumn::getIsCompleted, ColumnView::setIsCompleted)
+                .addMapping(col -> col.getTasks().stream().map(source -> modelMapper.map(source, TaskView.class)).toList(), ColumnView::setTasks);
     }
 }
