9dba2afccd96d33bc5c099051a4c91343c688856
farneser <farneser1@gmail.com>
2023-11-25 19:03:02 +0300

    feat: message service and send register message

--- src/main/java/dev/farneser/tasktracker/api/operations/queries/task/getbyid/GetTaskByIdQuery.java
+++ src/main/java/dev/farneser/tasktracker/api/operations/queries/task/getbyid/GetTaskByIdQuery.java
@@ -1,7 +1,6 @@
 package dev.farneser.tasktracker.api.operations.queries.task.getbyid;
 
 import dev.farneser.tasktracker.api.mediator.Query;
-import dev.farneser.tasktracker.api.operations.views.ColumnView;
 import dev.farneser.tasktracker.api.operations.views.TaskView;
 import lombok.AllArgsConstructor;
 import lombok.Data;
