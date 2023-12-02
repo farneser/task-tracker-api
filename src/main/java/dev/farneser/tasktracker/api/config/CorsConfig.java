e837d4b89e5cb09ec7492000f646745d15e7548c
farneser <farneser1@gmail.com>
2023-12-02 19:19:00 +0300

    fix: cors configuration

--- src/main/java/dev/farneser/tasktracker/api/config/CorsConfig.java
+++ src/main/java/dev/farneser/tasktracker/api/config/CorsConfig.java
@@ -2,14 +2,20 @@ package dev.farneser.tasktracker.api.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
+@EnableWebMvc
 public class CorsConfig {
     @Value("${application.allowed.origins:http://localhost:3000, http://client:3000}")
     private String[] allowedOrigins;
@@ -26,11 +32,11 @@ public class CorsConfig {
     }
 
     @Bean
-    public CorsFilter corsFilter() {
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
         var source = new UrlBasedCorsConfigurationSource();
         var config = new CorsConfiguration();
 
-        for (var allowedOrigin : allowedOrigins) {
+        for (String allowedOrigin : allowedOrigins) {
             config.addAllowedOrigin(allowedOrigin);
         }
 
@@ -39,15 +45,17 @@ public class CorsConfig {
         config.addAllowedMethod("PUT");
         config.addAllowedMethod("PATCH");
         config.addAllowedMethod("DELETE");
-        config.addAllowedHeader("Origin");
-        config.addAllowedHeader("Content-Type");
-        config.addAllowedHeader("Accept");
         config.setAllowCredentials(true);
+        config.setAllowedHeaders(List.of("*"));
 
         source.registerCorsConfiguration("/api/**", config);
 
+        var bean = new FilterRegistrationBean<>(new CorsFilter(source));
+
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
         printCorsConfiguration(config);
 
-        return new CorsFilter(source);
+        return bean;
     }
 }
