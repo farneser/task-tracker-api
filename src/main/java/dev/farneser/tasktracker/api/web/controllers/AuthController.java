a5305d856d90c5cdf1ddda19384752a3cc4ba1f5
farneser <farneser1@gmail.com>
2023-12-04 05:53:29 +0300

    feat: endpoints swagger annotations

--- src/main/java/dev/farneser/tasktracker/api/web/controllers/AuthController.java
+++ src/main/java/dev/farneser/tasktracker/api/web/controllers/AuthController.java
@@ -5,6 +5,9 @@ import dev.farneser.tasktracker.api.service.auth.AuthService;
 import dev.farneser.tasktracker.api.web.dto.auth.JwtDto;
 import dev.farneser.tasktracker.api.web.dto.auth.LoginRequest;
 import dev.farneser.tasktracker.api.web.dto.auth.RegisterDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,16 +25,31 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully authenticated user"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
+    @ApiOperation(value = "Authenticate", notes = "Authenticates a user")
     public ResponseEntity<JwtDto> authenticate(@RequestBody @Valid LoginRequest loginDto) throws NotFoundException {
         return ResponseEntity.ok(authService.authenticate(loginDto));
     }
 
     @PostMapping("/register")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully registered user"),
+            @ApiResponse(responseCode = "409", description = "Failed to register user")
+    })
+    @ApiOperation(value = "Register", notes = "Registers a user")
     public ResponseEntity<JwtDto> register(@RequestBody @Valid RegisterDto registerDto) throws InternalServerException, UniqueDataException {
         return ResponseEntity.ok(authService.register(registerDto));
     }
 
     @PostMapping("/refresh")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully refreshed auth token"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
+    @ApiOperation(value = "Refresh auth token", notes = "Refreshes the auth token for a user")
     public ResponseEntity<JwtDto> refresh(@RequestBody @Valid JwtDto jwtDto) throws TokenExpiredException, InvalidTokenException, NotFoundException {
         return ResponseEntity.ok(authService.refresh(jwtDto));
     }
