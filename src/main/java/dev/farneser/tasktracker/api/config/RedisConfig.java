3ae1168f81105fb6102eef319f53a1e85dfb4595
farneser <farneser1@gmail.com>
2023-12-05 03:09:08 +0300

    feat: redis config

---
+++ src/main/java/dev/farneser/tasktracker/api/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package dev.farneser.tasktracker.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.redis.host:localhost}")
+    private String redisHost;
+
+    @Value("${spring.redis.port:6379}")
+    private int redisPort;
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        var redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
+
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        var template = new RedisTemplate<String, Object>();
+
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return template;
+    }
+}
