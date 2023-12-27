0486ff28867856998a9880cb94f3e32f6ed62f7d
farneser <farneser1@gmail.com>
2023-12-27 08:15:08 +0300

    chore: application runner log remote servers running

--- src/main/java/dev/farneser/tasktracker/api/AppStartupRunner.java
+++ src/main/java/dev/farneser/tasktracker/api/AppStartupRunner.java
@@ -1,14 +1,17 @@
 package dev.farneser.tasktracker.api;
 
-import dev.farneser.tasktracker.api.exceptions.InternalServerException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.net.Inet4Address;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -16,36 +19,54 @@ public class AppStartupRunner implements ApplicationRunner {
     @Value("${server.port}")
     private Integer port;
 
-    private String getAddress() throws InternalServerException {
-        try (final var datagramSocket = new DatagramSocket()) {
-            datagramSocket.connect(InetAddress.getByName("1.1.1.1"), 80);
+    public List<String> getNetworkInfo(int port) {
+        var result = new ArrayList<String>();
 
-            return datagramSocket.getLocalAddress().getHostAddress();
-        } catch (Exception e) {
-            throw new InternalServerException("Remote address not found");
+        try {
+            var networkInterfaces = NetworkInterface.getNetworkInterfaces();
+
+            var interfaceList = Collections.list(networkInterfaces);
+
+            interfaceList.sort((ni1, ni2) -> ni1.getName().compareToIgnoreCase(ni2.getName()));
+
+            for (var networkInterface : interfaceList) {
+                if (!networkInterface.isUp() || networkInterface.isLoopback() || networkInterface.isVirtual()) {
+                    continue;
+                }
+
+                var addresses = networkInterface.getInetAddresses();
+
+                while (addresses.hasMoreElements()) {
+                    var address = addresses.nextElement();
+
+                    if (address instanceof Inet4Address) {
+                        result.add("http://" + address.getHostAddress() + ":" + port + "/");
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            log.error("Failed to get network interfaces", e);
         }
+
+        return result;
     }
 
     @Override
     public void run(ApplicationArguments args) {
-        var servers = 2;
+        var remoteAddresses = getNetworkInfo(port);
+
         var localMessage = "Server running local:\thttp://localhost:" + port;
-        var remoteMessage = new StringBuilder();
 
-        try {
-            remoteMessage.append("Server running remote:\thttp://").append(getAddress()).append(":").append(port);
-        } catch (InternalServerException e) {
-            remoteMessage = new StringBuilder("Failed to run server remote");
+        var remoteMessages = new ArrayList<String>();
+        remoteAddresses.forEach(address -> remoteMessages.add("Server running remote:\t" + address));
 
-            servers--;
-        }
-
-        var maxLength = Math.max(localMessage.length(), remoteMessage.length());
+        var servers = 1 + remoteAddresses.size();
+        var maxLength = Math.max(localMessage.length(), remoteMessages.stream().mapToInt(String::length).max().orElse(0));
 
         log.info("=".repeat(maxLength / 2 - 1) + " STATUS " + "=".repeat(maxLength / 2 - 1));
         log.info("Available: " + servers + " urls");
         log.info(localMessage);
-        log.info(remoteMessage.toString());
+        remoteMessages.forEach(log::info);
         log.info("=".repeat(maxLength + 6));
     }
 }
