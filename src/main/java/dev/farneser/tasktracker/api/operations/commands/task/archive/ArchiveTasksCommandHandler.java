d626b0bd6fcd0362939543fd93c7ce426904572a
farneser <farneser1@gmail.com>
2024-04-03 01:34:46 +0300

    chore: archive all user tasks

--- src/main/java/dev/farneser/tasktracker/api/operations/commands/task/archive/ArchiveTasksCommandHandler.java
+++ src/main/java/dev/farneser/tasktracker/api/operations/commands/task/archive/ArchiveTasksCommandHandler.java
@@ -1,8 +1,10 @@
 package dev.farneser.tasktracker.api.operations.commands.task.archive;
 
 import dev.farneser.tasktracker.api.mediator.CommandHandler;
-import dev.farneser.tasktracker.api.models.KanbanColumn;
-import dev.farneser.tasktracker.api.repository.ColumnRepository;
+import dev.farneser.tasktracker.api.models.Status;
+import dev.farneser.tasktracker.api.models.project.ProjectMember;
+import dev.farneser.tasktracker.api.repository.ProjectMemberRepository;
+import dev.farneser.tasktracker.api.repository.StatusRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -14,22 +16,30 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class ArchiveTasksCommandHandler implements CommandHandler<ArchiveTasksCommand, Void> {
-    private final ColumnRepository columnRepository;
+    private final StatusRepository statusRepository;
+    private final ProjectMemberRepository projectMemberRepository;
 
     @Override
     public Void handle(ArchiveTasksCommand command) {
-        List<KanbanColumn> columns = columnRepository.findByUserIdOrderByOrderNumber(command.getUserId()).orElse(new ArrayList<>());
 
-        log.debug("Columns found: {}", columns);
+        List<ProjectMember> projects = projectMemberRepository
+                .findProjectMemberByMemberId(command.getUserId())
+                .orElse(new ArrayList<>());
 
-        columns.forEach(column -> {
+        List<Status> statuses = new ArrayList<>();
+
+        projects.forEach(p -> statuses.addAll(p.getProject().getStatuses()));
+
+        log.debug("Columns found: {}", statuses);
+
+        statuses.forEach(column -> {
             log.debug("Column found: {}", column);
 
             if (column.getIsCompleted() && column.getTasks() != null) {
                 column.getTasks().forEach(task -> {
                     log.debug("Task found: {}", task);
 
-                    task.setColumn(null);
+                    task.setStatus(null);
                     task.setOrderNumber(-1L);
                 });
             }
@@ -37,7 +47,7 @@ public class ArchiveTasksCommandHandler implements CommandHandler<ArchiveTasksCo
 
         log.debug("Tasks archived");
 
-        columnRepository.saveAll(columns);
+        statusRepository.saveAll(statuses);
 
         return null;
     }
