7df3449646df360dbb9a3f1e6af4a5afa4d8eb13
farneser <farneser1@gmail.com>
2024-04-11 22:46:42 +0300

    feat: patch project member

---
+++ src/main/java/dev/farneser/tasktracker/api/operations/commands/project/patchmember/PatchProjectMemberCommand.java
@@ -0,0 +1,18 @@
+package dev.farneser.tasktracker.api.operations.commands.project.patchmember;
+
+import dev.farneser.tasktracker.api.mediator.Command;
+import dev.farneser.tasktracker.api.models.project.ProjectMember;
+import dev.farneser.tasktracker.api.models.project.ProjectRole;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PatchProjectMemberCommand implements Command<ProjectMember> {
+    private Long userId;
+    private Long memberId;
+    private Long projectId;
+    private ProjectRole role;
+}
---
+++ src/main/java/dev/farneser/tasktracker/api/operations/commands/project/patchmember/PatchProjectMemberCommandHandler.java
@@ -0,0 +1,56 @@
+package dev.farneser.tasktracker.api.operations.commands.project.patchmember;
+
+import dev.farneser.tasktracker.api.exceptions.NotFoundException;
+import dev.farneser.tasktracker.api.exceptions.OperationNotAuthorizedException;
+import dev.farneser.tasktracker.api.mediator.CommandHandler;
+import dev.farneser.tasktracker.api.models.project.ProjectMember;
+import dev.farneser.tasktracker.api.models.project.ProjectPermission;
+import dev.farneser.tasktracker.api.models.project.ProjectRole;
+import dev.farneser.tasktracker.api.repository.ProjectMemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PatchProjectMemberCommandHandler implements CommandHandler<PatchProjectMemberCommand, ProjectMember> {
+    private final ProjectMemberRepository projectMemberRepository;
+
+    @Override
+    public ProjectMember handle(PatchProjectMemberCommand command) throws NotFoundException, OperationNotAuthorizedException {
+        Long projectId = command.getProjectId();
+        Long adminId = command.getUserId();
+        Long memberId = command.getMemberId();
+        ProjectRole newRole = command.getRole();
+
+        ProjectMember admin = findProjectMember(projectId, adminId);
+        ProjectMember member = findProjectMember(projectId, memberId);
+
+        if (member.getRole().hasPermission(ProjectPermission.CREATOR_PATCH)) {
+            throw new OperationNotAuthorizedException("You can't change permission for this user");
+        }
+
+        if (admin.getRole() == ProjectRole.MEMBER) {
+            throw new OperationNotAuthorizedException("You can't change permissions in this project");
+        }
+
+        if (admin.getRole() == ProjectRole.CREATOR && newRole == ProjectRole.CREATOR) {
+            admin.setRole(ProjectRole.ADMIN);
+        }
+
+        if (admin.getRole() != ProjectRole.CREATOR || newRole != ProjectRole.CREATOR) {
+            member.setRole(newRole);
+        }
+
+        projectMemberRepository.save(admin);
+        projectMemberRepository.save(member);
+
+        return null;
+    }
+
+    private ProjectMember findProjectMember(Long projectId, Long memberId) throws NotFoundException {
+        return projectMemberRepository.findProjectMemberByProjectIdAndMemberId(projectId, memberId)
+                .orElseThrow(() -> new NotFoundException("Project member not found"));
+    }
+}
